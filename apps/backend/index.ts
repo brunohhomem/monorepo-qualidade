22aad4d95a25c2aefa4bdfe4360a3c65f876440e
brunohhomem <brunohhomem@gmail.com>
2025-04-30 18:03:15 -0300

    feat: aula 02 - add commons

---
+++ apps/backend/index.ts
@@ -0,0 +1,17 @@
+import http from 'http'
+import { httpClient } from '@brunohhomem/commons-http-client'
+
+http
+  .createServer(async (req, res) => {
+    const githubPayload = await httpClient.get(
+      'https://api.github.com/users/brunohhomem'
+    )
+
+    res.write(
+      JSON.stringify({ message: 'Hello from the server side!', githubPayload })
+    )
+    res.end()
+  })
+  .listen(4000)
+
+console.log('node js!')
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,3 +1,10 @@
+'use client'
+import { httpClient } from '@brunohhomem/commons-http-client'
+
 export default function Page() {
+  httpClient
+    .get('https://api.github.com/users/brunohhomem')
+    .then(data => console.log(data))
+
   return <h1>Hello World</h1>
 }
